b3672aeffa6ab9ff9932e702ceb934714c0ae270
NOUMIDIA-EL-AYOUBI <noumidiaelayoubi@gmail.com>
2022-03-25 10:10:33 +0100

    exercice2

--- TPC1S/exer2.c
+++ TPC1S/exer2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /* 
-    --Write a C program that allows you to swap the content of two integer variables by passing through a third auxiliary variable.
-    --Display the two variables before and after permutation.
+    Write a C program that allows you to swap the content of two integer variables by passing through a third auxiliary variable.
+    Display the two variables before and after permutation.
 */
 
 int main(int x, int y)
